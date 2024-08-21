2a5a13290b0c392ebd80362f46e1f7817621bc46
JuanGuzmendoza <juanguzman10102005@gmail.com>
2024-08-21 10:21:17 -0500

    Arreglo de excel que detecta cambios de años

--- app/Imports/DataImport.php
+++ app/Imports/DataImport.php
@@ -17,8 +17,21 @@ class DataImport implements ToCollection,WithHeadingRow
         unset($rows[1]);
         $mes = date('m');
         $año = date('Y');
+        $año2 = date('Y');
+        $año3 = date('Y');
         $mes2=$mes+1;
         $mes3=$mes+2;
+      if ($mes == 11) {
+            $mes3=1;
+            $año3++;
+       }
+      if($mes >=12){
+            $mes2=1;
+            $año2++;
+            $mes3=2;
+            $año3++;
+       }
+
              foreach($rows as $row) {
             Fechasentrega::create( [
                'cliente'=>$row[0],
@@ -34,7 +47,7 @@ class DataImport implements ToCollection,WithHeadingRow
                 'nombre'=>$row[2],
                 'cant'=>$row[4],
                 'mes'=>$mes2,
-                'año'=>$año
+                'año'=>$año2
              ]);
              Fechasentrega::create( [
                 'cliente'=>$row[0],
@@ -42,7 +55,7 @@ class DataImport implements ToCollection,WithHeadingRow
                 'nombre'=>$row[2],
                 'cant'=>$row[5],
                 'mes'=>$mes3,
-                'año'=>$año
+                'año'=>$año3
              ]);
            }
     }
